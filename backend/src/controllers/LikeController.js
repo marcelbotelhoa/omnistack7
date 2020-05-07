beec3e4084a9efd0a6536b1b258e7b7c1cce8e50
Marcel Botelho <marcelbotelho1@hotmail.com>
2020-05-07 18:14:48 -0300

    Mobile pronto para codar

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -4,12 +4,10 @@ module.exports = {
    async store(req, res) {
       const post = await Post.findById(req.params.id);
 
-      post.like += 1;
-
+      post.likes += 1;
       await post.save();
 
       req.io.emit('like', post);
-
       return res.json(post);
    }
 };
